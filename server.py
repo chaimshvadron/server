8e1ba5d3784310710161d5d52684d0325f08a163
Chaim Shvadron <150259430+chaimshvadron@users.noreply.github.com>
2025-03-16 01:49:17 +0200

    Update server.py

--- server.py
+++ server.py
@@ -13,9 +13,9 @@ def proxy():
     if not url:
         return "Missing URL", 400
 
-    scraper = cloudscraper.create_scraper()
+    scraper = cloudscraper.create_scraper(browser={'browser': 'chrome', 'platform': 'windows', 'mobile': False})
     headers = {
-        "User-Agent": "Mozilla/5.0"
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
     }
 
     try:
