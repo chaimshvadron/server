6d9202a5b092d8e0bf8b52ae25f2b7ef6a87d60b
Chaim Shvadron <150259430+chaimshvadron@users.noreply.github.com>
2025-03-16 01:44:01 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,7 +1,11 @@
-from flask import Flask, request
-import requests
+from flask import Flask, request, jsonify
+import cloudscraper
 
 app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Proxy Server is Running"
 
 @app.route('/proxy', methods=['GET'])
 def proxy():
@@ -9,15 +13,16 @@ def proxy():
     if not url:
         return "Missing URL", 400
 
+    scraper = cloudscraper.create_scraper()
     headers = {
         "User-Agent": "Mozilla/5.0"
     }
 
     try:
-        response = requests.get(url, headers=headers, timeout=10)
+        response = scraper.get(url, headers=headers, timeout=10)
         return response.text, response.status_code
     except Exception as e:
         return str(e), 500
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=8080)
