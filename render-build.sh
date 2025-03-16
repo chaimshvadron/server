d45c2a565eb6d111738634de6396cbec9cd72bc4
Chaim Shvadron <150259430+chaimshvadron@users.noreply.github.com>
2025-03-16 02:06:27 +0200

    Create render-build.sh

---
+++ render-build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Installing Chrome..."
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+sudo apt update
+sudo apt install -y google-chrome-stable
